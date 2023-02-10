9fb455061d977d183c37eee0ad7a84a0cc13c8cb
srgb001 <renangustavo.bs@gmail.com>
2023-02-10 01:03:06 -0300

    responsive -under construction- message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import {AboutComponent} from "./about/about.component";
 import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
-  // {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent}
 ];
